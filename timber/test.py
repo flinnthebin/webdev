7bab09733389cdb5135c1f111721674813f95e64
felix <47100787+flinnthebin@users.noreply.github.com>
2023-01-17 19:01:56 +1100

    created scraper class, classname appears universal across bunnings website.

--- timber/test.py
+++ timber/test.py
@@ -4,25 +4,30 @@ import time
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
-url = "https://www.bunnings.com.au/90-x-45mm-outdoor-framing-mgp10-h3-treated-pine-5-4m_p8032182"
+class Scraper:
 
-# create a browser instance
-browser = webdriver.Firefox()
+  def retrieve_html(self, url: str) -> str:
+    # open browser, retrieve html
+    browser = webdriver.Firefox()
+    browser.get(url)
+    # allow javascript to render elements
+    time.sleep(5)
+    # create html variable, close browser
+    html = browser.page_source
+    browser.quit()
+    return html
+  
+  def soup_finder(self, html: str, tag: str, classname: str) -> str:
+    soup = BeautifulSoup(html, 'lxml')
+    price = soup.find(tag, class_=classname).text
+    return price
 
-# load the website
-browser.get(url)
+url = "https://www.bunnings.com.au/90-x-45mm-framing-mgp12-h2-blue-pine-6-0m_p8031448"
+tag = "p"
+classname = "sc-cb9c4042-3 iGDFOw"
 
-# wait for the JavaScript to execute
-time.sleep(5)
-
-# extract the html source
-html = browser.page_source
-
-# close the browser
-browser.quit()
-
-soup = BeautifulSoup(html, 'lxml')
-
-price = soup.find("p", class_="sc-cb9c4042-3 iGDFOw").text
+scraper = Scraper()
+html = scraper.retrieve_html(url)
+price = scraper.soup_finder(html, tag, classname)
 
 print(price)
